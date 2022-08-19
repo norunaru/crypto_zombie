cc89d05e3ce1f256216889ed593e26b626227e0d
Remix Workspace <>
2022-08-19 23:23:02 +0900

    Lesson 1 Finished

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -1,5 +1,32 @@
 pragma solidity ^0.4.19;
 
 contract ZombieFactory {
-    
+
+    event NewZombie(uint zombieId, string name, uint dna); //id는 몇 번째 좀비인지를 알려주는 배열의 인덱스 번호
+
+    uint dnaDigits = 16; //좀비의 DNA는 16자리 숫자
+    //상태 변수는 컨트랙트 저장소에 영구적으로 저장
+    uint dnaModulus = 10 ** dnaDigits; //모듈로 연산에 사용하여 16자리보다 큰 숫자를 16자리로 만들어줌
+
+    struct Zombie { //좀비 구조체 선언 - 이름, DNA
+        string name;
+        uint dna;
+    }
+
+     Zombie[] public zombies; //public은 다른 컨트랙트에서 이 컨트랙트를 읽을 수 있음(쓸 수는 없음)
+
+     function _createZombie(string _name, uint _dna) private {  //private함수의 이름은 _로 시작이 관례
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;  //좀비(구조체)를 만들어 구조체 배열 zombies에 넣어주는 함수.
+        NewZombie(id, _name, _dna); //이벤트 실행
+    } //view - 함수가 데이터를 보기만 하고 변경하지 않음. pure - 함수가 앱에서 어떤 데이터도 접근하지 않음.
+
+    function _generateRandomDna(string _str) private view returns (uint) { //해싱을 통해 랜덤 DNA생성함수.
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+     function createRandomZombie(string _name) public  { //이름을 받아 16자리 랜덤 DNA숫자를 받고 이름과 DNA로 구조체를 만들어 구조체 배열에 push
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
 }
