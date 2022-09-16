43ac0849029c528bd6c39fcdb4d70b90961fdb6d
Remix Workspace <>
2022-09-16 18:09:25 +0900

    chapter 3

--- contracts/zombiefactory.sol
+++ contracts/zombiefactory.sol
@@ -1,16 +1,21 @@
 pragma solidity ^0.4.19;
 
-contract ZombieFactory {
+import "./ownable.sol";
+
+contract ZombieFactory is Ownable {
 
     event NewZombie(uint zombieId, string name, uint dna); //id는 몇 번째 좀비인지를 알려주는 배열의 인덱스 번호
 
     uint dnaDigits = 16; //좀비의 DNA는 16자리 숫자
     //상태 변수는 컨트랙트 저장소에 영구적으로 저장
     uint dnaModulus = 10 ** dnaDigits; //모듈로 연산에 사용하여 16자리보다 큰 숫자를 16자리로 만들어줌
+    uint cooldownTime = 1 days; //day아니고 days로 써야함
 
     struct Zombie { //좀비 구조체 선언 - 이름, DNA
         string name;
         uint dna;
+        uint32 level;
+        uint32 readyTime; //공격 쿨타임
     }
 
      Zombie[] public zombies; //public은 다른 컨트랙트에서 이 컨트랙트를 읽을 수 있음(쓸 수는 없음)
@@ -20,7 +25,7 @@ contract ZombieFactory {
 
      function _createZombie(string _name, uint _dna) internal {  //private함수의 이름은 _로 시작이 관례
        
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;  //좀비(구조체)를 만들어 구조체 배열 zombies에 넣어주는 함수.
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;  //좀비(구조체)를 만들어 구조체 배열 zombies에 넣어주는 함수.
         zombieToOwner[id] = msg.sender; //msg.sender - 현재 함수를 호출한 사람의 주소 반환
         ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna); //이벤트 실행
@@ -37,3 +42,6 @@ contract ZombieFactory {
         _createZombie(_name, randDna);
     }
 }
+
+//솔리디티는 seconds, minutes, hours, days, weeks, years 같은 시간 단위 또한 포함. 해당하는 길이 만큼의 초 단위 uint 숫자로 변환
+//솔리디티는 시간을 다룰 수 있는 단위계를 기본적으로 제공. now 변수를 쓰면 현재의 유닉스 타임스탬프 값을 얻을 수 있음
--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -19,19 +19,33 @@ contract KittyInterface { //인터페이스 선언 - 컨트랙트 선언 형태,
 
 
 contract ZombieFeeding is ZombieFactory {
-    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d; //크립토키티 컨트랙트 주소
-    KittyInterface kittyContract = KittyInterface(ckAddress);  //이제 'kittyContract'은 다른 컨트랙트 주소(ckAddress)를 가리킴.
+    
+    KittyInterface kittyContract;
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
+    function setKittyContractAddress(address _address) external onlyOwner{
+    kittyContract = KittyInterface(_address);
+  }
+
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+ 
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+      return (_zombie.readyTime <= now);
+  }
+
+
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal {
     require(msg.sender == zombieToOwner[_zombieId]); //좀비의 주인인지 확인
     Zombie storage myZombie = zombies[_zombieId];
-
+    require(_isReady(myZombie));
      _targetDna = _targetDna % dnaModulus; //targetDNA를 16자리로 맞춰주고
     uint newDna = (myZombie.dna + _targetDna) / 2; //원래 좀비와 타겟의 DNA 평균을 내서 새로운 DNA로
     if(keccak256(_species) == keccak256("kitty")) { //고양이가 먹이일 경우
         newDna = newDna - newDna % 100 + 99;        //마지막 두 자리 99로 변경
     }
     _createZombie("NoName", newDna); //새로운 좀비 생성
+    _triggerCooldown(myZombie);
     
   }
 
@@ -49,3 +63,9 @@ contract ZombieFeeding is ZombieFactory {
 //인터페이스를 정의하는 방법이 컨트랙트를 정의하는 것과 유사 
 //다른 컨트랙트와 상호작용하고자 하는 함수만을 선언할 뿐 다른 함수나 상태 변수를 언급하지 않음.
 //다음으로, 함수 몸체를 정의하지 않지. 중괄호 {, }를 쓰지 않고 함수 선언을 세미콜론(;)으로 간단하게 끝내지
+
+//chapter 3
+//컨트랙트로 배포한 최초의 코드는 항상 블록체인에 영구적으로 존재
+//Constructor :  컨트랙트와 동일한 이름을 가진,생략할 수 있는 특별한 함수
+//Modifier : 함수들에 대한 접근을 제어하기 위해 사용되는 일종의 유사 함수. 함수 실행 전의 요구사항 충족 여부를 확인하는 데에 사용
+//구조체 내에서는 uint타입을 작은 것으로 설정시 GAS 소모를 줄일 수 있음. +같은 크기로 설정시 더 줄일 수 있음
